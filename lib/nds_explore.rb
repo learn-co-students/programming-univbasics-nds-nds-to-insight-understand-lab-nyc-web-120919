c9b129ea658983e7aceafcd886191ac7bedbf3f9
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-05 21:04:48 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,8 +8,18 @@ def pretty_print_nds(nds)
   nil
 end
 
+#def print_first_directors_movie_titles
+#  directors_database[0][:movies].each do |movie|
+#    puts movie[:title]
+# end
+#end
+
 def print_first_directors_movie_titles
-  directors_database[0][:movies].each do |movie|
-    puts movie[:title]
- end
+  i = 0 
+  while i < directors_database.length 
+  print directors_database[i]
+  
+  i += 1 
+end
+  
 end
